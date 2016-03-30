32d014caa72e909b795b0eac34244e25f5bdea2b
Mihael Konjevic <konjevic@gmail.com>
2016-03-30 14:03:59 +0200

    Initial commit

---
+++ component.js
@@ -0,0 +1,8 @@
+import {Component} from "angular2/core";
+
+export default ng.core.Component({
+	selector: 'component-a',
+	template: '<div>foo</div>'
+}).Class({
+	constructor: function(){}
+});
---
+++ index.js
@@ -0,0 +1,4 @@
+import ComponentA from "./component";
+import bootstrap from "angular2/platform/browser";
+
+bootstrap(ComponentA);
